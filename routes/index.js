286a59a678d5c3664d2ddaacd66e8e672d6a31ec
Ryan Sy <ryansy@HP400.mynetworksettings.com>
2023-08-31 13:36:57 -0400

    adding LP frames functionality

--- routes/index.js
+++ routes/index.js
@@ -4,23 +4,23 @@ const { requiresAuth } = require('express-openid-connect');
 const axios = require('axios');
 
 // if authenticated, go to home page with search bar, else go to landing page
-router.get('/', function(req, res, next) {
-  req.oidc.isAuthenticated() ? 
-  res.redirect('home') :
-  res.render('index');
+router.get('/', function (req, res, next) {
+  req.oidc.isAuthenticated() ?
+    res.redirect('home') :
+    res.render('index');
 });
 
 // go to home page with search bar
 router.get('/home', requiresAuth(), (req, res) => {
-  res.render('home', {message: req.flash('success')});
+  res.render('home', { message: req.flash('success') });
 });
 
-// search discogs and display results
-router.post('/search-results', requiresAuth(), (req, res) => {
+// for coasters, search discogs and display results
+router.post('/search-coasters', requiresAuth(), (req, res) => {
   axios({
     method: 'get',
     url: 'https://api.discogs.com/database/search',
-    headers: {'User-Agent': 'RecordCoasters/1.0'},
+    headers: { 'User-Agent': 'RecordCoasters/1.0' },
     params: {
       q: req.body.searchTerm,
       country: 'US',
@@ -30,14 +30,14 @@ router.post('/search-results', requiresAuth(), (req, res) => {
       secret: process.env.DISCOGS_CONSUMER_SECRET
     }
   })
-  .then(function(response) {
-    const results = response.data.results;
-    res.render('search-results', { results: results} );
-  })
-  .catch(function(error) {
-    console.log('Error searching Discogs database:', error);
-    res.send('Error searching Discogs database');
-  });
+    .then(function (response) {
+      const results = response.data.results;
+      res.render('search-results', { results: results });
+    })
+    .catch(function (error) {
+      console.log('Error searching Discogs database:', error);
+      res.send('Error searching Discogs database');
+    });
 });
 
 // search discogs for specific release via resource_url
@@ -45,7 +45,7 @@ router.post('/details', requiresAuth(), (req, res) => {
   axios({
     method: 'get',
     url: req.body.resource_url,
-    headers: {'User-Agent': 'RecordCoasters/1.0'},
+    headers: { 'User-Agent': 'RecordCoasters/1.0' },
     params: {
       country: 'US',
       type: 'all',
@@ -54,36 +54,109 @@ router.post('/details', requiresAuth(), (req, res) => {
       secret: process.env.DISCOGS_CONSUMER_SECRET
     }
   })
-  .then(function(response) {
-    const result = response.data;
-    const resultObj = {
-      result: result,
-      artist: result.artists[0].name,
-      title: result.title
+    .then(function (response) {
+      const result = response.data;
+      const resultObj = {
+        result: result,
+        artist: result.artists[0].name,
+        title: result.title,
+        isCoaster: true
+      };
+      res.render('details', resultObj);
+    })
+    .catch(function (error) {
+      console.log('Error getting Discogs release details:', error);
+      res.send('Error getting Discogs release details');
+    });
+});
+
+// for LP frames
+router.post('/search-frames', async function (req, res) {
+  // search discogs for LP info/image
+  const imageData = await axios({
+      method: 'get',
+      url: 'https://api.discogs.com/database/search',
+      headers: { 'User-Agent': 'RecordCoasters/1.0' },
+      params: {
+        q: req.body.searchTerm,
+        country: 'US',
+        type: 'all',
+        format: 'vinyl',
+        key: process.env.DISCOGS_CONSUMER_KEY,
+        secret: process.env.DISCOGS_CONSUMER_SECRET
+      }
+    })
+    .then(function(response) {
+      const cover_image = response.data.results[0].cover_image;
+      const title = response.data.results[0].title;
+      return {
+        cover_image: cover_image,
+        title: title
+      }
+    })
+    .catch(function(error) {
+      console.log(error);
+      res.send('error searching data on discogs');
+    });
+  
+  const cover_image = imageData.cover_image;
+  const title = imageData.title;
+
+  // use hcti.io API to create LP cover image within a black frame
+  async function createFrame() {
+    const payload = {
+      html: `<div style="height: 100px; width: 100px; border: 10px solid #000000;"><img style="height: 100px; width: 100px;" src="${cover_image}" alt="${title}"></div>`
     };
-    res.render('details', resultObj);
-  })
-  .catch(function(error) {
-    console.log('Error getting Discogs release details:', error);
-    res.send('Error getting Discogs release details');
-  });
+
+    const headers = {
+      auth: {
+        username: process.env.HCTI_USER_ID,
+        password: process.env.HCTI_API_KEY
+      },
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+
+    const hctiResponse = await axios.post('https://hcti.io/v1/image', JSON.stringify(payload), headers);
+    const image_url = hctiResponse.data.url;
+
+    return image_url;
+  }
+
+  const image = await createFrame()
+    .then(function(response) {
+      return response;
+    })
+    .catch(function(error) {
+      res.send('error creating frame');
+    })
+  
+  const resultObj = {
+    title: title,
+    image: image,
+    isCoaster: false,
+  };
+
+  res.render('details', resultObj);
+
 });
 
 // save to shopify
 router.post('/save', requiresAuth(), (req, res) => {
   const productData = req.body;
   let tags = productData.tags;
-  if (typeof(tags) === 'object') {
+  if (typeof (tags) === 'object') {
     tags = tags.join();
   }
 
   // create product
   axios({
-    method:'post',
+    method: 'post',
     url: `https://${process.env.SHOPIFY_STORE_NAME}.myshopify.com/admin/api/2023-07/products.json`,
     headers: {
       'Content-Type': 'application/json',
-      'X-Shopify-Access-Token': process.env.SHOPIFY_ADMIN_API_ACCESS_TOKEN 
+      'X-Shopify-Access-Token': process.env.SHOPIFY_ADMIN_API_ACCESS_TOKEN
     },
     data: {
       product: {
@@ -101,32 +174,32 @@ router.post('/save', requiresAuth(), (req, res) => {
       }
     }
   })
-  .then(function(response) {
-    // update inventory item to 'tracked' = true
-    const inventoryItemId = response.data.product.variants[0].inventory_item_id;
+    .then(function (response) {
+      // update inventory item to 'tracked' = true
+      const inventoryItemId = response.data.product.variants[0].inventory_item_id;
 
-    return axios({
-      method:'put',
-      url: `https://${process.env.SHOPIFY_STORE_NAME}.myshopify.com/admin/api/2023-07/inventory_items/${inventoryItemId}.json`,
-      headers: {
-        'Content-Type': 'application/json',
-        'X-Shopify-Access-Token': process.env.SHOPIFY_ADMIN_API_ACCESS_TOKEN 
-      },
-      data: {
-        inventory_item: {
-          tracked: true
+      return axios({
+        method: 'put',
+        url: `https://${process.env.SHOPIFY_STORE_NAME}.myshopify.com/admin/api/2023-07/inventory_items/${inventoryItemId}.json`,
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Shopify-Access-Token': process.env.SHOPIFY_ADMIN_API_ACCESS_TOKEN
+        },
+        data: {
+          inventory_item: {
+            tracked: true
+          }
         }
-      }
+      });
+    })
+    .then(function (response) {
+      req.flash('success', `"${req.body.title}" successfully saved to Shopify.`);
+      res.redirect('home');
+    })
+    .catch(function (error) {
+      console.log('Error saving to Shopify:', error);
+      res.send('Error saving to Shopify');
     });
-  })
-  .then(function(response) {
-    req.flash('success', `"${req.body.title}" successfully saved to Shopify.`);
-    res.redirect('home');
-  })
-  .catch(function(error) {
-    console.log('Error saving to Shopify:', error);
-    res.send('Error saving to Shopify');
-  });
 });
 
 module.exports = router;
